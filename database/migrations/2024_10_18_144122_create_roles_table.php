61bf9b15dd7d0e526376caac059f7fa790152727
tarum <block-code@mail.ru>
2024-10-18 17:55:28 +0300

    create roles

--- database/migrations/2024_10_18_144122_create_roles_table.php
+++ database/migrations/2024_10_18_144122_create_roles_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
+            $table->string('role', 255);
+
+            $table->unsignedBigInteger('user_id');
+
+            $table->foreign('user_id')->references('id')->on('users');
+
             $table->timestamps();
         });
     }
